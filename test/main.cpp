b92e327d0e58d5bee9ef0177a4211f94c4a99a36
huyifan <yifan_hu@zju.edu.cn>
2019-10-31 23:10:11 +0800

    feat: 搭建初步测试文件架构

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by huyifan on 2019/10/31.
+//
+
+#include<torch/script.h>
+#include<iostream>
+#include<memory>
+using namespace std;
+
+int main(){
+
+   cout << "test built environment" << endl;
+
+    return 0;
+}
