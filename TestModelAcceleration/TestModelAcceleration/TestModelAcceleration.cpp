ccc939891389ce86ae26503e3e67527b667cb3aa
huyifan <yifan_hu@zju.edu.cn>
2019-11-02 14:27:46 +0800

    fixbug: 重新构建架构

---
+++ TestModelAcceleration/TestModelAcceleration/TestModelAcceleration.cpp
@@ -0,0 +1,12 @@
+﻿// TestModelAcceleration.cpp: 定义应用程序的入口点。
+//
+
+#include "TestModelAcceleration.h"
+
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ TestModelAcceleration/TestModelAcceleration/TestModelAcceleration.h
@@ -0,0 +1,9 @@
+﻿// TestModelAcceleration.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <torch/script.h>
+#include <memory>
+// TODO: 在此处引用程序需要的其他标头。
